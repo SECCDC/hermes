ba1ba6d857a8a13ef377d6f55cdfe5c2a9c1adf8
Bren Briggs <code@fraq.io>
2021-01-22 21:09:45 -0500

    Add basic products list route

--- hermes/server.go
+++ hermes/server.go
@@ -2,6 +2,7 @@ package hermes
 
 import (
 	"fmt"
+	"hermes/model"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,6 +12,8 @@ func Run() {
 	fmt.Println("Starting Hermes...")
 	r := gin.Default()
 	r.GET("/healthz", healthCheck)
+	r.GET("/api/v1/products", listProducts)
+
 	r.Run()
 
 	defer fmt.Println("Goodbye!")
@@ -19,5 +22,13 @@ func Run() {
 func healthCheck(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"status": "ok",
+	})
+}
+
+func listProducts(c *gin.Context) {
+	c.JSON(http.StatusOK, model.Product{
+		Name:        "reticulated spline",
+		Price:       44.50,
+		Description: "It reticulates the splines",
 	})
 }
